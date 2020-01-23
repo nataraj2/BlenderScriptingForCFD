3a33086090c6687412be679559ff3282ce96c711
nataraj2 <nataraj2@illinois.edu>
2020-01-22 18:29:50 -0700

    Simplified version - .blend file for scene

--- BlenderCyclesTest.py
+++ BlenderCyclesTest.py
@@ -3,60 +3,7 @@ import bpy
 bpy.context.scene.render.engine = 'CYCLES'
 
 bpy.ops.object.delete(use_global=False)
-bpy.ops.mesh.primitive_plane_add(view_align=False, enter_editmode=False, location=(0, 0, 0), layers=(True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False))
-bpy.ops.transform.resize(value=(30, 30, 30), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-bpy.ops.transform.rotate(value=1.57, axis=(1.0, 0.0, 0.0), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-bpy.ops.transform.translate(value=(10.0, 10.0, -0.0), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
-
-obj_camera = bpy.context.scene.camera
-obj_camera.location[0]=10.0
-obj_camera.location[1]=-10.0
-obj_camera.location[2]=0.0
-obj_camera.rotation_mode='XYZ'
-
-obj_camera.rotation_euler[0]=1.57
-obj_camera.rotation_euler[1]=0.0
-obj_camera.rotation_euler[2]=0.523
-
-obj_camera.scale[0]=10
-obj_camera.scale[1]=10
-obj_camera.scale[2]=10
-
-# Get material
-mat_plane = bpy.data.materials.get("Plane")
-if mat_plane is None:
-    # create material
-    mat_plane = bpy.data.materials.new(name="Plane")
-
-mat_plane.use_nodes=True
-nodes=mat_plane.node_tree.nodes
-for node in nodes:
-    nodes.remove(node)
-
-# create emission node
-node_emission = nodes.new(type='ShaderNodeEmission')
-node_emission.inputs[0].default_value = (0,1,0,1)  # green RGBA
-node_emission.inputs[1].default_value = 4.0 # strength
-node_emission.location = 0,0
-
-# create output node
-node_output = nodes.new(type='ShaderNodeOutputMaterial')
-#node_output.location = 100,0
-node_checker=nodes.new(type='ShaderNodeTexChecker')
-node_checker.inputs[2].default_value=(0,0,0,1)
-node_checker.inputs[3].default_value=20
-# link nodes
-links = mat_plane.node_tree.links
-link = links.new(node_emission.inputs[0], node_checker.outputs[0])
-link = links.new(node_emission.outputs[0], node_output.inputs[0])
-# Get material
-ob = bpy.context.active_object
-if ob.data.materials:
-    # assign to 1st material slot
-    ob.data.materials[0] = mat_plane
-else:
-    # no slots
-    ob.data.materials.append(mat_plane)
+bpy.ops.wm.open_mainfile(filepath="DropScene.blend")
 
 for i in range(0,5,1):
 
@@ -82,6 +29,9 @@ for i in range(0,5,1):
 	link = links.new(refractionshader.outputs[0], mixshader.inputs[1])
 	link = links.new(glossyshader.outputs[0], mixshader.inputs[2])
 	link = links.new(mixshader.outputs[0], dropoutput.inputs[0])
+	#objname = "Plic Mesh %02d"%i
+	#bpy.data.objects[objname].scale[0]=1.0
+
 	 # Get material
 	ob = bpy.context.active_object
 	if ob.data.materials:
